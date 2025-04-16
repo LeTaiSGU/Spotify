260564c76a76845b71d514f7fba419b90732f91b
TruongDHiep <truonghiep051103@gmail.com>
2025-04-16 18:18:07 +0700

    chinh sua giao dien va route

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,38 +1,23 @@
 import React from "react";
 import BottomPlayer from "./BottomPlayer";
 import "./layout.css";
-import PlaylistContent from "../playlist/playlistcontent";
 import Rightbar from "../sidebar/rightbar/rightbar";
 import Sidebar from "../sidebar/leftbar/sidebar";
 import TopBar from "./TopBar";
+import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <div className="flex flex-col h-screen bg-black text-white">
-      {/* Top Bar */}
-      <div className="fixed top-0 left-0 w-full z-10">
-        <TopBar />
-      </div>
-
-      {/* Main Content */}
-      <div className="flex flex-1 overflow-hidden pt-16 pb-16">
-        {/* Sidebar */}
+    <div className="layout">
+      <TopBar />
+      <div className="main-container">
         <Sidebar />
         <div className="main-content">
-          <PlaylistContent />
-          {/* <ContentPlaylist /> */}
-          {/* <UserProfile /> */}
           <Outlet />
         </div>
-
-        {/* Rightbar */}
         <Rightbar />
       </div>
-
-      {/* Bottom Player */}
-      <div className="fixed bottom-0 left-0 w-full z-10">
-        <BottomPlayer />
-      </div>
+      <BottomPlayer />
     </div>
   );
 };
