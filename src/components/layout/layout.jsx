45203d2f7a48de89a1a8e4441c10fcdbe6a33ea7
LeTaiSGU <letaikun11112@gmail.com>
2025-05-05 22:13:17 +0700

    pull main

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import BottomPlayer from "./BottomPlayer";
 import "./layout.css";
 import Rightbar from "../sidebar/rightbar/rightbar";
@@ -13,11 +13,8 @@ const Layout = () => {
   const isRightbarVisible = useSelector(
     (state) => state.songs.isRightbarVisible
   );
-
-  const [showChat, setShowChat] = useState(false); // Mặc định chưa mở ChatBox
-
   return (
-    <div className="flex flex-col h-screen bg-black text-white">
+    <div className="layout">
       <TopBar />
       <div className="main-container">
         <Sidebar />
@@ -26,24 +23,7 @@ const Layout = () => {
         </div>
         {selectedSong && isRightbarVisible && <Rightbar />}
       </div>
-
-      <div className="fixed bottom-0 left-0 w-full z-10">
-        <BottomPlayer />
-      </div>
-
-      <div className="fixed bottom-20 right-4 z-50">
-        {showChat ? (
-          <ChatBox onClose={() => setShowChat(false)} />
-        ) : (
-          <button
-            onClick={() => setShowChat(true)}
-            className="bg-[#0084ff] text-white w-12 h-12 rounded-full shadow-lg hover:bg-blue-600 transition"
-            title="Mở chat"
-          >
-            💬
-          </button>
-        )}
-      </div>
+      <BottomPlayer />
     </div>
   );
 };
