9ea6f6e5db9a7814b78169df2a376dd1c436c56b
Truong Dai Hiep <132136901+TruongDHiep@users.noreply.github.com>
2025-05-15 18:53:27 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,31 +6,14 @@ import { analyzer } from "vite-bundle-analyzer"; // Sửa từ visualizer thành
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react(), analyzer()], // Sử dụng analyzer thay vì visualizer
-  base: "./",
   server: {
     port: 3000,
     hmr: true, // Bật Hot Module Replacement để tăng tốc dev
   },
   resolve: {
-    alias: [{ find: "~", replacement: "/src" }],
+    alias: [
+      { find: '~', replacement: '/src' }
+    ]
   },
-  build: {
-    outDir: "dist",
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name]-[hash].js`,
-        chunkFileNames: `assets/[name]-[hash].js`,
-        assetFileNames: `assets/[name]-[hash].[ext]`,
-        manualChunks(id) {
-          if (id.includes("node_modules")) return "vendor";
-          if (id.includes("playlist") || id.includes("contentPlaylist")) {
-            return "playlist";
-          }
-        },
-      },
-      chunkFileNames: "assets/[name]-[hash].js", // Tên file chunk rõ ràng
-    },
-    minify: "esbuild", // Nén mã nguồn để giảm kích thước
-    sourcemap: false, // Tắt sourcemap trong production (dùng trong dev nếu cần debug)
-  },
+  base: ''
 });
