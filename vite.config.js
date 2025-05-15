574f3551e5f51e65b53cb25b562b18484a5359fe
LeTaiSGU <letaikun11112@gmail.com>
2025-05-15 20:14:30 +0700

    pull baronee

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,35 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { analyzer } from "vite-bundle-analyzer"; // Sửa từ visualizer thành analyzer
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react(), analyzer()], // Sử dụng analyzer thay vì visualizer
-  base: "/",
-  server: {
-    port: 3000,
-    hmr: true, // Bật Hot Module Replacement để tăng tốc dev
+  base: "./", // hoặc "" tùy routing
+  plugins: [tailwindcss(), react()],
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+    minify: "esbuild",
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name]-[hash].js",
+        chunkFileNames: "assets/[name]-[hash].js",
+        assetFileNames: "assets/[name]-[hash].[ext]",
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+          if (id.includes("playlist") || id.includes("contentPlaylist")) {
+            return "playlist";
+          }
+        },
+      },
+    },
   },
   resolve: {
     alias: [{ find: "~", replacement: "/src" }],
   },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return "vendor"; // Tách dependencies ra chunk riêng
-          }
-          if (id.includes("playlist") || id.includes("contentPlaylist")) {
-            return "playlist"; // Tách các component playlist ra chunk riêng
-          }
-        },
-      },
-      chunkFileNames: "assets/[name]-[hash].js", // Tên file chunk rõ ràng
-    },
-    minify: "esbuild", // Nén mã nguồn để giảm kích thước
-    sourcemap: false, // Tắt sourcemap trong production (dùng trong dev nếu cần debug)
+  server: {
+    port: 3000,
+    hmr: true,
   },
 });
